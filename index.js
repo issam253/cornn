1daaa3c6a9f9c2c0a4d3582064dae15705e58e7f
hakerman <142625808+hakerman-008@users.noreply.github.com>
2024-05-28 03:58:44 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,29 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
-
 async function fetchXNXXURL(urlXNXX) {
-    const options = {
-        method: 'POST',
-        url: 'https://all-media-downloader1.p.rapidapi.com/xnxx',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '719775e815msh65471c929a0203bp10fe44jsndcb70c04bc42',
-            'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
-        },
-        data: {
-            urlXNXX: urlXNXX
-        }
-    };
+    const apiUrl = `https://tools.betabotz.eu.org/tools/xnxxdl?url=${encodeURIComponent(urlXNXX)}`;
 
-    const response = await axios.request(options);
-    const { result } = response.data;
-    if (result && result.url) {
-        return result.url;
-    } else {
+    try {
+        const response = await axios.get(apiUrl);
+        const { result } = response.data;
+        if (result && result.url) {
+            return result.url;
+        } else {
+            throw new Error('Failed to fetch XNXX URL');
+        }
+    } catch (error) {
         throw new Error('Failed to fetch XNXX URL');
     }
 }
 
-
 app.get('/kshitiz', async (req, res) => {
     try {
         const urlXNXX = req.query.url;
-
         if (urlXNXX) {
-           
             const xnxxURL = await fetchXNXXURL(urlXNXX);
             return res.json({ xnxxURL });
         } else {
-           
             return res.status(400).json({ error: 'Query parameter "url" is required' });
         }
     } catch (error) {
@@ -44,7 +32,6 @@ app.get('/kshitiz', async (req, res) => {
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
